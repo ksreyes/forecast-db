c9cc69454341260751181fa1bb072ee221173401
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2025-11-06 15:41:47 +0100

    Edit filters lead-in text

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,36 +1,23 @@
-import './Filters.css';
-import FilterCountry from './FilterCountry.jsx';
-import FilterIndicator from './FilterIndicator.jsx';
-import FilterLead from './FilterLead.jsx';
+import "./Filters.css";
+import FilterCountry from "./FilterCountry.jsx";
+import FilterIndicator from "./FilterIndicator.jsx";
+import FilterLead from "./FilterLead.jsx";
 
 function Filters({ filters, setFilters }) {
-
-
-    return (
-        <div
-            className="filters-container fill-blue pad-t-5 pad-b-5 pad-x-5 flex-col"
-        >
-            <div>
-                <h2 className="mgn-b-4 text-white text-size-2 text-bold">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </h2>
-            </div>
-            <div className="filters flex-row">
-                <FilterCountry
-                    filters={filters} 
-                    setFilters={setFilters}
-                />
-                <FilterIndicator 
-                    filters={filters} 
-                    setFilters={setFilters}
-                />
-                <FilterLead 
-                    filters={filters}
-                    setFilters={setFilters}
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className="filters-container fill-blue pad-t-5 pad-b-5 pad-x-5 flex-col">
+      <div>
+        <h2 className="mgn-b-4 text-white text-size-2 text-bold">
+          Visualize forecasts of environmental risks by adjusting the filters below.
+        </h2>
+      </div>
+      <div className="filters flex-row">
+        <FilterCountry filters={filters} setFilters={setFilters} />
+        <FilterIndicator filters={filters} setFilters={setFilters} />
+        <FilterLead filters={filters} setFilters={setFilters} />
+      </div>
+    </div>
+  );
 }
 
 export default Filters;
