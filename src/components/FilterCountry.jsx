688431576c3eea9aaf2b88e263844730171cb2a4
ksreyes <kenneth.rejes@gmail.com>
2025-11-16 12:04:31 +0100

    Update country filters list

--- src/components/FilterCountry.jsx
+++ src/components/FilterCountry.jsx
@@ -1,32 +1,37 @@
 export default function FilterCountry({ filters, setFilters }) {
+    const options = [
+        "Ethiopia", 
+        "Mozambique", 
+        "Niger",
+        "Nigeria",
+        "Zimbabwe"
+    ];
 
-  const options = ["Ethiopia", "Mozambique", "Nigeria"];
-
-  const handleChange = (event) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      country: event.target.value,
-    }));
-  };
+    const handleChange = (event) => {
+        setFilters((prevFilters) => ({
+            ...prevFilters,
+            country: event.target.value,
+        }));
+    };
 
   return (
     <div className="filter country-filter">
-      <h2 htmlFor="country-filter" className="filter-label block">
-        Country
-      </h2>
+        <h2 htmlFor="country-filter" className="filter-label block">
+            Country
+        </h2>
 
-      <select
-        id="country-filter"
-        value={filters.country}
-        onChange={handleChange}
-        className="mgn-t-1"
-      >
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+        <select
+            id="country-filter"
+            value={filters.country}
+            onChange={handleChange}
+            className="mgn-t-1"
+        >
+            {options.map((option) => (
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
     </div>
   );
 }
